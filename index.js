204a85ed89b0aab943e420492054eb597ce77179
catzilla <catzillaorz@outlook.com>
2022-10-10 20:33:17 +0800

    feat: parsing req body

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 // run `node index.js` in the terminal
+var fs = require('fs')
+var http = require('http');
+const server = http.createServer((req, res) => {
+    const url = req.url;
+    const method = req.method;
+    if (url === '/') return res.end();
+})
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+server.listen(8080)
