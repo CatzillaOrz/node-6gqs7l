89f6637dc4d8a0e1dd1b748563568e7b5b32ad0a
catzilla <catzillaorz@outlook.com>
2022-10-13 16:46:33 +0800

    feat: express router filter and redirect

--- index.js
+++ index.js
@@ -6,7 +6,10 @@ const adminRouter = require('./routes/admin');
 const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}))
-app.use(adminRouter)
+app.use('/admin', adminRouter)
 app.use(shopRouter)
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1>Page not found!</h1>')
+})
 app.listen(8080)
--- routes/admin.js
+++ routes/admin.js
@@ -2,11 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send('<form action="/prodect" method="POST"><input type="text" name="title"><button type="submit">Add Prodect</button></form>')
+    res.send('<form action="/admin/add-prodect" method="POST"><input type="text" name="title"><button type="submit">Add Prodect</button></form>')
 })
 
 
-router.post('/prodect', (req, res, next) => {
+router.post('/add-prodect', (req, res, next) => {
     console.log(req.body);
     res.redirect('/')
 })
