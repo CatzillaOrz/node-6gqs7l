71d8543d8d79524324d8ec19c484dd572b5cfec0
catzilla <catzillaorz@outlook.com>
2022-10-13 16:29:09 +0800

    feat: expressjs router and midware

--- index.js
+++ index.js
@@ -1,21 +1,12 @@
 // run `node index.js` in the terminal
 const express = require('express')
-const bodyParser = require('express')
+const bodyParser = require('express');
 const app = express();
+const adminRouter = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended: false}))
-
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/prodect" method="POST"><input type="text" name="title"><button type="submit">Add Prodect</button></form>')
-})
-
-app.use('/prodect', (req, res, next) => {
-    console.log(req.body);
-    res.redirect('/')
-})
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>hello from express!<h1>')
-})
+app.use(adminRouter)
+app.use(shopRouter)
 
 app.listen(8080)
