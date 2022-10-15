0a5267b7097c9f2bf01e1ef53601396223a48bee
catzilla <catzillaorz@outlook.com>
2022-10-15 22:52:18 +0800

    feat: add page not found for 404 status res

--- app.js
+++ app.js
@@ -10,6 +10,6 @@ app.use(adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found!</h1>");
+  res.status(404).sendFile(__dirname, "views", "404.html");
 });
 app.listen(8080);
