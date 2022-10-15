ff7f89f1d8010cdf3b37d3dd4fad5d502a9d6f4b
catzilla <catzillaorz@outlook.com>
2022-10-15 23:48:03 +0800

    feat: add rootDir util

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // run `node index.js` in the terminal
 const express = require("express");
 const bodyParser = require("express");
+const rootDir = require("./util/path");
 const app = express();
 const adminRouter = require("./routes/admin");
 const shopRouter = require("./routes/shop");
@@ -10,6 +11,6 @@ app.use(adminRouter);
 app.use(shopRouter);
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(__dirname, "views", "404.html");
+  res.status(404).sendFile(rootDir, "views", "404.html");
 });
 app.listen(8080);
