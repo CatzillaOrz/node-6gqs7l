6569915fa2da78436181b04f545efe35dd372298
catzilla <catzillaorz@outlook.com>
2022-10-23 23:37:25 +0800

    feat: shop product structor

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,7 +1,8 @@
 const Product = require("../models/product");
+
 exports.getAddProduct = (req, res, next) => {
   //res.sendFile(path.join(rootDir, "views", "add-product.html"));
-  res.render("add-product", {
+  res.render("admin/add-product", {
     path: "admin",
     pageTitle: "Add-Product",
     activeProduct: true,
@@ -13,17 +14,4 @@ exports.postAddProduct = (req, res, next) => {
   const product = new Product(req.body.title);
   product.save();
   res.redirect("/");
-};
-
-exports.getProducts = (req, res, next) => {
-  Product.fetchAll((products) => {
-    res.render("shop", {
-      prods: products,
-      pageTitle: "Shop",
-      path: "shop",
-      hasProducts: products.length > 0,
-      activeShop: true,
-      productCss: true,
-    });
-  });
 };
---
+++ controllers/shop.js
@@ -0,0 +1,53 @@
+const Product = require("../models/product");
+
+exports.getAddProduct = (req, res, next) => {
+  //res.sendFile(path.join(rootDir, "views", "add-product.html"));
+  res.render("admin/add-product", {
+    path: "admin",
+    pageTitle: "Add-Product",
+    activeProduct: true,
+    productCss: true,
+  });
+};
+
+exports.postAddProduct = (req, res, next) => {
+  const product = new Product(req.body.title);
+  product.save();
+  res.redirect("/");
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop Index",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+    activeProduct: true,
+    productCss: true,
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
