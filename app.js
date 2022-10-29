6060f1ccf6019bbca603da0b404218f88402cc30
catzilla <catzillaorz@outlook.com>
2022-10-29 16:39:00 +0800

    feat: mysql connection test case

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 // run `node index.js` in the terminal
 const path = require("path");
 const rootDir = require("./util/path");
-
+const db = require("./util/database");
 const express = require("express");
 const bodyParser = require("express");
 
@@ -14,10 +14,18 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.use(express.static(path.join(rootDir, "public")));
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use("/admin", adminRoutes.routes);
 app.use(shopRouter);
 
 app.use(errorController.get404);
+
+db.execute("SELECT * FROM products")
+  .then((result) => {
+    console.log(result[0], result[1]);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 app.listen(8080);
