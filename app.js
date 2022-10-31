d659439eeb8adaea165b82a997444a8ff0032ecd
catzilla <catzillaorz@outlook.com>
2022-10-31 16:35:33 +0800

    feat: User Module | Product.belongsTo User | User.hasMany(Product) | sync({force: treu})

--- app.js
+++ app.js
@@ -1,31 +1,36 @@
 // run `node index.js` in the terminal
-const path = require("path");
-const rootDir = require("./util/path");
-const express = require("express");
-const bodyParser = require("express");
+const path = require('path');
+const rootDir = require('./util/path');
+const express = require('express');
+const bodyParser = require('express');
 
-const sequelize = require("./util/database");
+const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
-const adminRoutes = require("./routes/admin");
-const shopRouter = require("./routes/shop");
+const adminRoutes = require('./routes/admin');
+const shopRouter = require('./routes/shop');
 
-const errorController = require("./controllers/error");
+const errorController = require('./controllers/error');
 
 const app = express();
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
-app.use(express.static(path.join(rootDir, "public")));
+app.use(express.static(path.join(rootDir, 'public')));
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use("/admin", adminRoutes.routes);
+app.use('/admin', adminRoutes.routes);
 app.use(shopRouter);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
 sequelize
-  .sync()
-  .then((results) => {
-    app.listen(8080);
-  })
-  .catch((err) => {
-    console.log(err);
-  });
+    .sync({ force: true })
+    .then((results) => {
+        app.listen(8080);
+    })
+    .catch((err) => {
+        console.log(err);
+    });
